c862e8148c35547acfdcb8300e85f2b823ee4783
士子☀️ <84996057@qq.com>
2023-04-02 15:00:56 +0800

    post

--- deploy.mjs
+++ deploy.mjs
@@ -8,6 +8,7 @@ const ossBucket = "oss://zy-blog-v2";
 const deployFalgs = ["-r", "-f", localDir, ossBucket];
 
 try {
+  await $`npm run build`;
   await $`ossutil64 cp ${deployFalgs}`;
 } catch (p) {
   console.log(chalk.red("执行失败，错误码是：", p.exitCode));
